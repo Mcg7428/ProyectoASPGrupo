f294cc94c32d73aeeb26ce6887672d371eb2360c
Jose Carlos Garcia Diaz <chepecarlos@alswblog.org>
2013-06-24 02:42:09 -0600

    Cambiando todos los botones por imagenes
    Problemas con los controles de validacion al pensar que tabien ellos
    tiene ue ser validados

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -4,54 +4,28 @@ Partial Class MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Session("Conectado") <> "Yes" Then
-            Response.Redirect("Default.aspx")
+            Response.Redirect("~/Default.aspx")
         ElseIf Session("Cargo") = "Empleado" Then
-            BMante.Enabled = True
-            BMante.Visible = True
+            IMante.Visible = True
         ElseIf Session("Cargo") = "SuperVisor" Then
-            BMante.Enabled = True
-            BMante.Visible = True
-            BAdmin.Enabled = True
-            BAdmin.Visible = True
+            IMante.Visible = True
+            IAdmin.Visible = True
         ElseIf Session("Cargo") = "Jefe" Then
-            BMante.Enabled = True
-            BMante.Visible = True
-            BAdmin.Enabled = True
-            BAdmin.Visible = True
-            BUsuario.Enabled = True
-            BUsuario.Visible = True
-            BEquipo.Enabled = True
-            BEquipo.Visible = True
+            IMante.Visible = True
+            IAdmin.Visible = True
+            IUsuario.Visible = True
+            IEquipo.Visible = True
         ElseIf Session("Cargo") = "SuperUsuario" Then
-            BEdificio.Enabled = True
-            BEdificio.Visible = True
-            BAdmin.Enabled = True
-            BAdmin.Visible = True
-            BUsuario.Enabled = True
-            BUsuario.Visible = True
-            BEquipo.Enabled = True
-            BEquipo.Visible = True
-            BMante.Enabled = True
-            BMante.Visible = True
+            IEdificio.Visible = True
+            IAdmin.Visible = True
+            IUsuario.Visible = True
+            IUsuario.Visible = True
+            IEquipo.Visible = True
+            IMante.Visible = True
         Else
-            Response.Redirect("Default.aspx")
+            Response.Redirect("~/Default.aspx")
         End If
     End Sub
 
-    Protected Sub BUsuario_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles BUsuario.Click ''boton que envia usuarios
-        Response.Redirect("DefaultUsuarios.aspx")
-    End Sub
-
-    Protected Sub BMante_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles BMante.Click ''boton que envia Mantennimiento
-        Response.Redirect("DefaultMantenimiento.aspx")
-    End Sub
-
-    Protected Sub BEdificio_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles BEdificio.Click ''boton que envia Inmuebles
-        Response.Redirect("DefaultInmuebles.aspx")
-    End Sub
-
-    Protected Sub BEquipo_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles BEquipo.Click ''boton que evia equipos
-        Response.Redirect("DefaultEquipos.aspx")
-    End Sub
 End Class
 
