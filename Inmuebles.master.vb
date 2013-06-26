3af833cdc3e14166c7f657e423d4521f95ae4666
Jose Carlos Garcia Diaz <chepecarlos@alswblog.org>
2013-06-26 01:05:08 -0600

    Mejorando apariencia de edificio

--- Inmuebles.master.vb
+++ Inmuebles.master.vb
@@ -1,5 +1,21 @@
 ﻿
 Partial Class Inmuebles
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Response.Redirect("~/RegistrarEdificio.aspx")
+    End Sub
+
+    Protected Sub Button2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Response.Redirect("~/RegistrarDepto.aspx")
+    End Sub
+
+    Protected Sub Button3_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Response.Redirect("~/RegistroSala.aspx")
+    End Sub
+
+    Protected Sub Button4_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Response.Redirect("~/RegistroLaboratorio.aspx")
+    End Sub
 End Class
 
