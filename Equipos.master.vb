a28005dfa5878237d6bf3e697f2f5ec02e8e4ac5
Jose Carlos Garcia Diaz <chepecarlos@alswblog.org>
2013-06-25 23:12:57 -0600

    Mejorar el haspecto de Equipos

--- Equipos.master.vb
+++ Equipos.master.vb
@@ -1,5 +1,13 @@
 ﻿
 Partial Class Equipos
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub ImageButton2_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton2.Click
+        Response.Redirect("~/RegistrarEquipo.aspx")
+    End Sub
+
+    Protected Sub ImageButton3_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton3.Click
+        Response.Redirect("~/EliminarActualizaEquipo.aspx")
+    End Sub
 End Class
 
