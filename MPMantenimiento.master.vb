ce3c8139beb80db088980a31842535b5b453df66
Jose Carlos Garcia Diaz <chepecarlos@alswblog.org>
2013-06-26 00:13:01 -0600

    Mejorando apariencia de mantenimientoo

--- MPMantenimiento.master.vb
+++ MPMantenimiento.master.vb
@@ -1,5 +1,17 @@
 ﻿
 Partial Class MPMantenimiento
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub ImageButton2_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton2.Click
+        Response.Redirect("~/CrearMantenimiento.aspx")
+    End Sub
+
+    Protected Sub ImageButton3_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton3.Click
+        Response.Redirect("~/CrearTarea.aspx")
+    End Sub
+
+    Protected Sub ImageButton1_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton1.Click
+        Response.Redirect("~/CrearSubTarea.aspx")
+    End Sub
 End Class
 
