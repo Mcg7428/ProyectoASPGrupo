fd38f530f49f34edfe624a08b2e815d428575d63
Jose Carlos Garcia Diaz <chepecarlos@alswblog.org>
2013-06-25 22:39:23 -0600

    Mejorando apariencia de master usuarios

--- Usuarios.master.vb
+++ Usuarios.master.vb
@@ -1,5 +1,13 @@
 ﻿
 Partial Class Usuarios
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub ImageButton2_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton2.Click
+        Response.Redirect("~/AgregarUsuario.aspx")
+    End Sub
+
+    Protected Sub ImageButton3_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles ImageButton3.Click
+        Response.Redirect("~/EliminarActualizar.aspx")
+    End Sub
 End Class
 
